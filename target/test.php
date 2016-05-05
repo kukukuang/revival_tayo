84910b9f2d9f9cb2674afeae4a2dd903f12260d2
kukukuang <shb3709@hanmail.net>
2016-05-05 18:28:14 +0900

    Added files via upload

---
+++ target/test.php
@@ -0,0 +1,24 @@
+﻿<?php  
+  
+$con=mysqli_connect("http://52.38.179.125","root","revival16","test");  
+  
+if (mysqli_connect_errno($con))  
+{  
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();  
+}  
+  
+  
+   
+$res = mysqli_query($con,"select * from Test");  
+   
+$result = array();  
+   
+while($row = mysqli_fetch_array($res)){  
+  array_push($result,  
+    array('id'=>$row[0],'name'=>$row[1],'address'=>$row[2]  
+    ));  
+}  
+   
+echo json_encode(array("result"=>$result));  
+mysqli_close($con);  
+?>
